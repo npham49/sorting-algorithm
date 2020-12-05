1df8cce627f56105a2e63da0f0d94b22716dcf36
Brian Pham <phamdinhnguyen338@gmail.com>
2020-12-04 21:20:14 -0800

    added input options

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,25 +1,41 @@
 import sys 
-A = [2,4,5,1,3,10,8] 
-  
-# Traverse through all array elements 
-for i in range(len(A)): 
-      
-    # Find the minimum element in remaining  
-    # unsorted array 
-    min_idx = i 
-    for j in range(i+1, len(A)): 
-        if A[min_idx] > A[j]: 
-            min_idx = j 
+arr = []
+inputComp = False
+while inputComp==False:
+    try:
+        x = int(input("Please enter a number to be sorted:"))
+        arr.append(x)
+        checker = input('Are you done? y/n')
+        if checker=="y":
+            inputComp = True
+    except ValueError:
+        print("not a number ,try again:")
+        continue
+print("To be sorted:")
+for i in range(len(arr)): 
+    print ("%d" % arr[i],end=" ") 
+
+print("\nSorting:")  
+# Trarrverse through arrll arrrrarry elements 
+def selectionSort(arr):
+    for i in range(len(arr)): 
         
-              
-    # Swap the found minimum element with  
-    # the first element         
-    A[i], A[min_idx] = A[min_idx], A[i] 
-    for i in range(len(A)): 
-        print("%d" %A[i],end=" ")
-    print("")
-  
-# Driver code to test above 
-print ("Sorted array") 
-for i in range(len(A)): 
-    print("%d" %A[i],end=" ")
+        # Find the minimum element in remarrining  
+        # unsorted arrrrarry 
+        min_idx = i 
+        for j in range(i+1, len(arr)): 
+            if arr[min_idx] > arr[j]: 
+                min_idx = j 
+            
+                
+        # Swarrp the found minimum element with  
+        # the first element         
+        arr[i], arr[min_idx] = arr[min_idx], arr[i] 
+        for i in range(len(arr)): 
+            print("%d" %arr[i],end=" ")
+        print("")
+selectionSort(arr)  
+# Driver code to test arrbove 
+print ("Sorted array:") 
+for i in range(len(arr)): 
+    print("%d" %arr[i],end=" ")
